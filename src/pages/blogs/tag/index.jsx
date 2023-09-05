6506bed48b6f3e7246b116973297c34f5d04631d
xup60521 <84887662+xup60521@users.noreply.github.com>
2023-09-05 21:07:42 +0800

    next.js remake

---
+++ src/pages/blogs/tag/index.jsx
@@ -0,0 +1,13 @@
+import HomeLayout from "@/pages/components/layout"
+
+const Tag = () => {
+    return (
+        <HomeLayout>
+            <div className="content">
+                redirect...
+            </div>
+        </HomeLayout>
+    )
+}
+
+export default Tag;
