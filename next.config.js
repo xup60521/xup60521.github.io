84103d0b77e74b9471d6dc2298bd883dcc1552dd
xup60521 <84887662+xup60521@users.noreply.github.com>
2023-09-05 21:16:49 +0800

    next.js remake

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
 
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/blogs/tag',
-        destination: '/blogs',
-        permanent: true,
-      },
-    ]
-  },
+  
 }
